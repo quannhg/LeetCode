fadf7d7d9e7ed63b0379b6310df82670078d3b32
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-01-13 17:58:03 +0700

    Time: 1 ms (81.00%), Space: 2.7 MB (34.27%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.rs
@@ -0,0 +1,32 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        match root {
+            Some(node) => {
+                let left_depth = Solution::max_depth(node.borrow().left.clone());
+                let right_depth = Solution::max_depth(node.borrow().right.clone());
+                1 + left_depth.max(right_depth)
+            }
+            None => 0
+        }
+    }
+}
