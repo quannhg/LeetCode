584834460242a23a66834d6e609873c888da47f6
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-01-16 19:54:20 +0700

    Time: 38 ms (5.00%), Space: 2.4 MB (20.00%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let mut moves_str = moves.clone();
+        let mut starting_point = (0, 0);
+        for direction in moves.chars() {
+            match direction {
+                'R' => starting_point.0 +=1,
+                'L' => starting_point.0 -=1,
+                'U' => starting_point.1 +=1,
+                'D' => starting_point.1 -=1,
+                _ => (),
+            }
+            moves_str.remove(0);
+        }
+        return starting_point.0 == 0 && starting_point.1 == 0
+    }
+}
