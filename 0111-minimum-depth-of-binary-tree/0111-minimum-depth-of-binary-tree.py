7c97b48bae8c6983d3bfdd0c06907cf912b467b4
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-01 09:58:52 +0700

    Time: 389 ms (5.03%), Space: 51.3 MB (6.32%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -5,16 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-
-        # Check if either left or right subtree is empty
-        if not root.left:
-            return 1 + self.minDepth(root.right)
-        elif not root.right:
-            return 1 + self.minDepth(root.left)
-
-        # If both subtrees are non-empty, return the minimum depth
-        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
-        
+    def minDepth(self, root):
+        if not root: return 0
+        d = list(map(self.minDepth, (root.left, root.right)))
+        return 1 + (min(d) or max(d))
