dfc84e93d96f53cba7185fb59dfa2d785fc94001
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-05 22:18:23 +0700

    Time: 159 ms (38.00%), Space: 18.9 MB (21.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,15 +1,19 @@
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        firstPointer, lastPointer, result = 0, len(nums) -1, []
-        while firstPointer < lastPointer:
-            if abs(nums[firstPointer]) >= (nums[lastPointer]):
-                result.insert(0, nums[firstPointer] ** 2)
-                firstPointer += 1
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        return_array = [0] * len(A)
+        write_pointer = len(A) - 1
+        left_read_pointer = 0
+        right_read_pointer = len(A) - 1
+        left_square = A[left_read_pointer] ** 2
+        right_square = A[right_read_pointer] ** 2
+        while write_pointer >= 0:
+            if left_square > right_square:
+                return_array[write_pointer] = left_square
+                left_read_pointer += 1
+                left_square = A[left_read_pointer] ** 2
             else:
-                result.insert(0, nums[lastPointer] ** 2)
-                lastPointer -= 1
-        result.insert(0, nums[firstPointer] ** 2)
-        
-        return result
-        
-        
+                return_array[write_pointer] = right_square
+                right_read_pointer -= 1
+                right_square = A[right_read_pointer] ** 2
+            write_pointer -= 1
+        return return_array
