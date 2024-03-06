d265613e3a76425be0e29507688d9175808c5b83
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-06 14:42:06 +0700

    Time: 33 ms (75.67%), Space: 16.5 MB (89.98%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,27 +1,28 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        def backspacePointer(pointer, s):
-            amount_character_remove = 0
-            while (s[pointer] == '#' or amount_character_remove > 0) and pointer >= 0:
-                if s[pointer] == '#':
-                    amount_character_remove += 1
+        def backspacePointer(pointer, string):
+            skip = 0
+            while pointer >= 0 and (string[pointer] == '#' or skip > 0):
+                if string[pointer] == '#':
+                    skip += 1
                 else:
-                    amount_character_remove -=1
-                pointer -=1
+                    skip -= 1
+                pointer -= 1
             return pointer
-                
-            
+
         s_pointer, t_pointer = len(s) - 1, len(t) - 1
+
         while s_pointer >= 0 or t_pointer >= 0:
-            
-            s_pointer, t_pointer = backspacePointer(s_pointer, s), backspacePointer(t_pointer, t)
+            s_pointer = backspacePointer(s_pointer, s)
+            t_pointer = backspacePointer(t_pointer, t)
+
             if s_pointer == -1 or t_pointer == -1:
                 return s_pointer == t_pointer
+
             if s[s_pointer] != t[t_pointer]:
                 return False
-            s_pointer -=1
-            t_pointer -=1
+
+            s_pointer -= 1
+            t_pointer -= 1
+
         return s_pointer == t_pointer
-            
-            
-        
