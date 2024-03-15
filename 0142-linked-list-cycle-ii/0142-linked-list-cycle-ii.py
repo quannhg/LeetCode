da100486b44cbafad4c7ffbb1587619477488eb1
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-15 09:07:06 +0700

    Time: 49 ms (23.60%), Space: 19.1 MB (81.53%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -1,10 +1,28 @@
-class Solution(object):
-    def detectCycle(self, head):
-        slow = fast = head
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    
+    
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+        # find the intersection
         while fast and fast.next:
             slow, fast = slow.next, fast.next.next
-            if slow == fast: break
-        else: return None  # if not (fast and fast.next): return None
+            if fast == slow:
+                break # fast and slow is in intersection
+        else:
+            return None
         while head != slow:
-            head, slow = head.next, slow.next
+            head, slow = head.next, slow.next    
         return head
+        
+        # x: distance from head to begining of the cicle -> slow move = x + y
+        # fast move = 2(x+y)
+        # 2(x+y) - (x+y) = N*C
+        # x = NC - y
+        # x + y + NC -y = x + NC
+        # head = slow 
