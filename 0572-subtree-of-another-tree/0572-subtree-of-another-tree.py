6251bb581ca3cdab9223cf137b39dea2dc74680a
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-16 21:01:11 +0700

    Time: 66 ms (86.69%), Space: 16.8 MB (51.95%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,13 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def isEqual(root1, root2):
-            if root1 == None or root2 == None: return root1 == root2
-            return root1.val == root2.val and isEqual(root1.left, root2.left) and isEqual(root1.right, root2.right)
-        def dfs(root, subRoot):
-            if root == None: return False
-            return isEqual(root, subRoot) or dfs(root.left, subRoot) or dfs(root.right, subRoot)
-        return dfs(root, subRoot)
+class Solution(object):
+    def isSubtree(self, s, t):
+        """
+        :type s: TreeNode
+        :type t: TreeNode
+        :rtype: bool
+        """
+        def convert(p):
+            return "^" + str(p.val) + "#" + convert(p.left) + convert(p.right) if p else "$"
+        
+        return convert(t) in convert(s)
         
