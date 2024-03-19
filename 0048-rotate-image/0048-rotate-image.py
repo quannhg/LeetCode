b4f0ed7e24bd4c35ede2583e50e3f74bb920fcb0
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-19 08:26:28 +0700

    Time: 41 ms (39.24%), Space: 16.6 MB (21.09%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,8 +3,5 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        matrix.reverse()
-        for row_idx in range(len(matrix)):
-            for col_idx in range(row_idx):
-                matrix[row_idx][col_idx],  matrix[col_idx][row_idx] = matrix[col_idx][row_idx], matrix[row_idx][col_idx]
+        matrix[:] = map(list, zip(*matrix[::-1]))
         
