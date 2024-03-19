b2ac41401190e018d473008916af8585a673efde
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-19 09:22:11 +0700

    Time: 2599 ms (88.40%), Space: 16.6 MB (27.43%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,34 @@
+class Solution:
+    def travel(self, board: List[List[str]], word: str, row_idx: int, col_idx: int) -> bool:
+        if board[row_idx][col_idx] != word[0]:
+            return False
+        if len(word) == 1:
+            return True
+        
+        board[row_idx][col_idx] = ""
+        
+        if (row_idx - 1 >= 0) and len(board[row_idx - 1][col_idx]) > 0:
+            if self.travel(board, word[1:], row_idx - 1, col_idx):
+                return True
+            
+        if (row_idx + 1 < len(board)) and len(board[row_idx + 1][col_idx]) > 0:
+            if self.travel(board, word[1:], row_idx + 1, col_idx):
+                return True
+            
+        if (col_idx - 1 >= 0) and len(board[row_idx][col_idx - 1]) > 0:
+            if self.travel(board, word[1:], row_idx, col_idx - 1):
+                return True
+            
+        if (col_idx + 1 < len(board[0])) and len(board[row_idx][col_idx + 1]) > 0:
+            if self.travel(board, word[1:], row_idx, col_idx + 1):
+                return True
+        
+        board[row_idx][col_idx] = word[0]
+        return False
+    
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        for row_idx in range(len(board)):
+            for col_idx in range(len(board[row_idx])):
+                if self.travel(board, word, row_idx, col_idx):
+                    return True
+        return False
