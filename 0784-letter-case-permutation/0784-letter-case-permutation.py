b108328366d0297a17725cd0706871582e1e7c53
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-20 12:02:26 +0700

    Time: 48 ms (84.29%), Space: 17.4 MB (70.78%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,12 +1,9 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        def casePermutationTraverse(cur_str):
-            cur_str_len = len(cur_str)
-            if cur_str_len == len(s):
-                return [cur_str]
-            
-            if s[cur_str_len].upper() != s[cur_str_len].lower():
-                return casePermutationTraverse(cur_str + s[cur_str_len].upper()) + casePermutationTraverse(cur_str + s[cur_str_len].lower())
-            return casePermutationTraverse(cur_str + s[cur_str_len])
-        return casePermutationTraverse("")
-        
+        permutation = [""]
+        for char in s:
+            if char.isalpha():
+                permutation = [perm + case for perm in permutation for case in [char.upper(), char.lower()]]
+            else:
+                permutation = [perm + char for perm in permutation]
+        return permutation
