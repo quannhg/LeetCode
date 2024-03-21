8223d50d6d0aa50df89c9a0b439004b9998723e6
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-21 15:35:11 +0700

    Time: 39 ms (50.96%), Space: 16.8 MB (43.05%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,14 +1,14 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        result = []
         def addParenthesis(amount_left, amount_right, cur_str):
-            if amount_left >= n:
-                if amount_right >= n:
-                    return [cur_str]
-                else:
-                    return addParenthesis(amount_left, amount_right + 1, cur_str + ')')
+            nonlocal result
+            if len(cur_str) == 2*n:
+                result.append(cur_str)
+            if amount_left < n:
+                addParenthesis(amount_left + 1, amount_right , cur_str + '(')
             if amount_left > amount_right:
-                return addParenthesis(amount_left + 1, amount_right, cur_str +'(') + addParenthesis(amount_left, amount_right+1, cur_str + ')')
-            else:
-                return addParenthesis(amount_left + 1, amount_right, cur_str + '(')
-        return addParenthesis(0, 0, "")
+                addParenthesis(amount_left, amount_right+1, cur_str + ')')
+        addParenthesis(0, 0, "")
+        return result
         
