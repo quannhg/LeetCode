2d9311e5f62a012dbefed991f52691be75f67043
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-03-31 13:20:09 +0700

    Time: 137 ms (83.40%), Space: 21.2 MB (66.93%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,11 +1,20 @@
 class Solution:
+    def countLessOrEqual(self, matrix, value):
+        num_element = 0
+        col_idx = len(matrix[0]) - 1 # start with the rightmost element
+        for row_idx in range(len(matrix)):
+            while col_idx >= 0 and matrix[row_idx][col_idx] > value: col_idx -= 1
+            num_element += col_idx + 1
+        return num_element
+    
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        matrix_edge_len = len(matrix)
-        min_heap = [] # [(val, row_idx, col_idx)]
-        for row_idx in range(min(matrix_edge_len, k)):
-            heappush(min_heap, (matrix[row_idx][0], row_idx, 0))
-        min_num = -1
-        for kthSmall in range(k):
-            min_num, row_idx, col_idx = heappop(min_heap)
-            if col_idx + 1 < matrix_edge_len: heappush(min_heap, (matrix[row_idx][col_idx + 1], row_idx, col_idx + 1))
-        return min_num
+        left, right = matrix[0][0], matrix[len(matrix) - 1][len(matrix) - 1]
+        kthSmallestNum = -1
+        while(left <= right):
+            mid = (left + right) // 2
+            if self.countLessOrEqual(matrix, mid) >= k:
+                kthSmallestNum = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return kthSmallestNum
