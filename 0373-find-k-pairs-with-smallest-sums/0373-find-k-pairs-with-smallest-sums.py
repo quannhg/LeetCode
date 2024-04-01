9f4543ff8ec5a7d18b0fc56423ceb615712200b1
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-01 09:02:34 +0700

    Time: 964 ms (79.83%), Space: 35.9 MB (80.32%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,25 +1,26 @@
 class Solution:
     def kSmallestPairs(self, nums1, nums2, k):
-        res = []
-        if not nums1 or not nums2 or not k:
-            return res
+        if not nums1 or not nums2:
+            return []
         
-        heap = []
-        visited = set()
+        min_heap, visited_pairs, smallest_pairs = [], set(), []
         
-        heapq.heappush(heap, (nums1[0] + nums2[0], 0, 0))
+        heappush(min_heap, (nums1[0] + nums2[0], 0, 0)) # (sum, idx_1, idx_2)
+        visited_pairs.add((0,0)) # (idx_1, idx_2)
         
-        visited.add((0, 0))
-        
-        while len(res) < k and heap:
-            _, i, j = heapq.heappop(heap)
-            res.append([nums1[i], nums2[j]])
+        while len(smallest_pairs) < k and min_heap:
+            _min_sum, nums1_idx, nums2_idx = heappop(min_heap)
+            smallest_pairs.append((nums1[nums1_idx], nums2[nums2_idx]))
             
-            if i+1 < len(nums1) and (i+1, j) not in visited:
-                heapq.heappush(heap, (nums1[i+1] + nums2[j], i+1, j))
-                visited.add((i+1, j))
+            if nums1_idx + 1 < len(nums1) and (nums1_idx + 1, nums2_idx) not in visited_pairs:
+                heappush(min_heap, (nums1[nums1_idx + 1] + nums2[nums2_idx], nums1_idx + 1, nums2_idx))
+                visited_pairs.add((nums1_idx + 1, nums2_idx))
+                
+            if nums2_idx + 1 < len(nums2) and (nums1_idx, nums2_idx + 1) not in visited_pairs:
+                heappush(min_heap, (nums1[nums1_idx] + nums2[nums2_idx + 1], nums1_idx, nums2_idx + 1))
+                visited_pairs.add((nums1_idx, nums2_idx + 1))
+                
+        return smallest_pairs
+
             
-            if j+1 < len(nums2) and (i, j+1) not in visited:
-                heapq.heappush(heap, (nums1[i] + nums2[j+1], i, j+1))
-                visited.add((i, j+1))
-        return res
+        
