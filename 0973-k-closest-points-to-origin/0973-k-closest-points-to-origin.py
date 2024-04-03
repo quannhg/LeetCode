4d8471196a1279a49c98e9ea191e7749ce0b9576
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-03 09:59:50 +0700

    Time: 7169 ms (5.00%), Space: 22.7 MB (81.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,11 +1,23 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        k_closest_points = []
-        for point in points:
-            distance = - (point[0]**2 + point[1]**2) # use negative to build max heap
-            if len(k_closest_points) == k:
-                heappushpop(k_closest_points, (distance, point[0], point[1]))
-            else:
-                heappush(k_closest_points, (distance, point[0], point[1]))
-        return [(x, y) for _, x, y in k_closest_points]
+        left, right = 0, len(points) - 1
+        while left <= right:
+            mid = self.partition(points, left, right)
+            if mid == k: break
+            elif mid < k: left = mid + 1
+            elif mid > k: right = mid - 1
+        return points[:k]
+    
+    def partition(self, points, left, right):
+        pivot = points[left]
+        while left < right:
+            while left < right and self.compareDistance(points[right], pivot) > 0: right -= 1
+            points[left] = points[right]
+            while left < right and self.compareDistance(points[left], pivot) <= 0: left += 1
+            points[right] = points[left]
+        points[left] = pivot
+        return left
+        
+    def compareDistance(self, point1, point2):
+        return point1[0]**2 + point1[1]**2 - point2[0]**2 - point2[1]**2
         
