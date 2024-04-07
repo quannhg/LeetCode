1800642189f0336bb9fc7c8ea080f4292a449bad
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-07 10:14:03 +0700

    Time: 27 ms (97.40%), Space: 16.5 MB (94.10%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal_triangle = []
+        for row_level in range(numRows):
+            new_level = [1] * (row_level + 1)
+            for idx in range(1, len(new_level) - 1):
+                new_level[idx] = pascal_triangle[row_level - 1][idx - 1] + pascal_triangle[row_level - 1][idx]
+            pascal_triangle += [new_level]
+        return pascal_triangle
+            
+        
