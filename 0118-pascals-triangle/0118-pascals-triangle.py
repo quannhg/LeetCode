eaa49cbd8e10d9162c3b5361fb7971b4edf61cf7
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-07 10:24:35 +0700

    Time: 16 ms (99.99%), Space: 16.7 MB (12.41%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         for row_level in range(numRows):
             new_level = [1] * (row_level + 1)
             for idx in range(1, len(new_level) - 1):
-                new_level[idx] = pascal_triangle[row_level - 1][idx - 1] + pascal_triangle[row_level - 1][idx]
+                new_level[idx] = pascal_triangle[-1][idx - 1] + pascal_triangle[-1][idx]
             pascal_triangle += [new_level]
         return pascal_triangle
             
