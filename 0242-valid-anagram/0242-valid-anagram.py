52f520b01bc77c63c9d78f89b834ed2cebfe4c26
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-09 10:09:09 +0700

    Time: 40 ms (92.07%), Space: 16.8 MB (91.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t): return False
+        
+        alphabet = [0] * 26
+        for char in s:
+            alphabet[ord(char) - ord('a')] += 1
+        for char in t:
+            alphabet[ord(char) - ord('a')] -= 1
+        
+        return all(count == 0 for count in alphabet)
         
