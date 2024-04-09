af685be669e6d8cc239dca427290e59c479bd778
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-09 09:51:41 +0700

    Time: 45 ms (42.89%), Space: 17.6 MB (96.49%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,3 +1,18 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        return map(lambda number: "Fizz" * (not number % 3) + "Buzz" * (not number % 5) or str(number), range(1, n + 1))
+        fizz = buzz = 0
+        string_array = []
+        for number in range(1, n + 1):
+            fizz += 1
+            buzz += 1
+            new_string = ""
+            if fizz == 3:
+                new_string += "Fizz"
+                fizz = 0
+            if buzz == 5:
+                new_string += "Buzz"
+                buzz = 0
+            if len(new_string) == 0:
+                new_string = str(number)
+            string_array += [new_string]
+        return string_array
