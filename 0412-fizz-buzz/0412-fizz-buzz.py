448f941f4b85f7ba758192b5e087577b93661625
Quan Nguyen <128902312+quannhg@users.noreply.github.com>
2024-04-09 09:43:45 +0700

    Time: 46 ms (34.31%), Space: 17.3 MB (99.65%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,13 +1,3 @@
 class Solution:
-    def receiveFizzBuzz(self, number: int) -> str:
-        if number % 3 == 0 and number % 5 == 0:
-            return "FizzBuzz"
-        elif number % 3 == 0:
-            return "Fizz"
-        elif number % 5 == 0:
-            return "Buzz"
-        else:
-            return str(number)
-            
     def fizzBuzz(self, n: int) -> List[str]:
-        return map(self.receiveFizzBuzz, range(1, n + 1))
+        return map(lambda number: "Fizz" * (not number % 3) + "Buzz" * (not number % 5) or str(number), range(1, n + 1))
